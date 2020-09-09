48c491018ec64e3eff0778f2c9006bc555063bd8
jiangxuzhi <1741585872@qq.com>
2020-09-09 22:00:06 +0800

    2020-9-9

--- Week_01/README.md
+++ Week_01/README.md
@@ -1,1 +1,15 @@
 学习笔记
+
+三数之和的心得：
+
+Arrays的asList方法：
+
+（1）该方法不适用于基本数据类型（byte,short,int,long,float,double,boolean）
+
+（2）该方法将数组与列表链接起来，当更新其中之一时，另一个自动更新
+
+（3）不支持add和remove方法
+
+一定要注意Arrays的asList()这个方法不适用于基本数据类型，并且这个方法返回的是Arrays类内置的arrayList类，这个内置类没有add()和remove()方法。坑
+
+解决方法：使用包装类。
---
+++ Week_01/本周作业/twoSum.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+    //map的key是数值，value是数组下标
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i = 0;i < nums.length;i++) {
+            if (map.containsKey(target - nums[i])){
+                return  new int[]{map.get(target - nums[i]),i};
+            }
+            map.put(nums[i],i);
+        }
+        throw new IllegalArgumentException();
+    }
+}
